ebcb0429796797efe52f81d4bdabe855fead98be
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ChartStackedSorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView1 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView2 = new DevExpress.XtraCharts.LineSeriesView();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisX.Range.SideMarginsEnabled = true;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.Range.SideMarginsEnabled = true;
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Location = new System.Drawing.Point(12, 12);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.PaletteName = "Flow";
+            series1.Name = "Series 1";
+            series1.View = lineSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.SeriesTemplate.View = lineSeriesView2;
+            this.chartControl1.Size = new System.Drawing.Size(970, 492);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 513);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 548);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+
+namespace ChartStackedSorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chartControl1.Series.Clear();
+
+            Series s1, s2, s3;
+
+            s1 = new Series("Serie1", ViewType.StackedBar);
+            s2 = new Series("Serie2", ViewType.StackedBar);
+            s3 = new Series("Serie3", ViewType.StackedBar);
+
+            Random r = new Random();
+
+            for(int i = 0; i < 10; i++) {
+                s1.Points.Add(new SeriesPoint(i, Math.Round(r.NextDouble() * 100)));
+                s2.Points.Add(new SeriesPoint(i, Math.Round(r.NextDouble() * 100)));
+                s3.Points.Add(new SeriesPoint(i, Math.Round(r.NextDouble() * 100)));
+            }
+
+            s1.LabelsVisibility = s2.LabelsVisibility = s3.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False;
+
+            chartControl1.Series.AddRange(new Series[] { s1, s2, s3 });
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            ChartStackedSorting css = new ChartStackedSorting(chartControl1);
+
+            css.SortChartSeries();
+        }
+    }
+
+  public class ChartStackedSorting {
+    private ChartControl chart;
+
+    public ChartStackedSorting(ChartControl chart) {
+        this.chart = chart;
+    }
+
+    public void SortChartSeries() {
+        for(int i = 0; i < chart.Series[0].Points.Count; i++) {
+            SeriesPoint p1 = chart.Series[0].Points[i];
+            SeriesPoint pointMax = p1;
+            int indexMax = i;
+
+            double secondItemsMax = GetAdditionalValues(p1);
+
+            for(int j = i + 1; j < chart.Series[0].Points.Count; j++) {
+                SeriesPoint p2 = chart.Series[0].Points[j];
+
+                double secondItems = GetAdditionalValues(p2);
+
+                if(p2.Values[0] + secondItems > pointMax.Values[0] + secondItemsMax) {
+                    pointMax = p2;
+                    secondItemsMax = secondItems;
+                    indexMax = j;
+                }
+            }
+
+            chart.Series[0].Points.Swap(p1, pointMax);
+            SwapAdditionalPoints(p1, pointMax);
+        }
+    }
+
+    private double GetAdditionalValues(SeriesPoint point) {
+        double result = 0.0;
+
+        for(int i = 1; i < chart.Series.Count; i++)
+            for(int j = 0; j < chart.Series[i].Points.Count; j++)
+                if(Convert.ToDouble(chart.Series[i].Points[j].Argument) == Convert.ToDouble(point.Argument) && chart.Series[i].Points[j] != point)
+                    result += chart.Series[i].Points[j].Values[0];
+
+        return result;
+    }
+
+    private void SwapAdditionalPoints(SeriesPoint pi, SeriesPoint pj) {
+        for(int s = 1; s < chart.Series.Count; s++) {
+
+            SeriesPoint swp1 = null;
+            SeriesPoint swp2 = null;
+            
+            for(int i = 0; i < chart.Series[s].Points.Count; i++) {
+                if(Convert.ToDouble(chart.Series[s].Points[i].Argument) == Convert.ToDouble(pi.Argument))
+                    swp1 = chart.Series[s].Points[i];
+                if(Convert.ToDouble(chart.Series[s].Points[i].Argument) == Convert.ToDouble(pj.Argument))
+                    swp2 = chart.Series[s].Points[i];
+            }
+
+            if(swp1 != null && swp2 != null)
+                chart.Series[s].Points.Swap(swp1, swp2);
+        }
+
+    }
+
+  }
+
+}
